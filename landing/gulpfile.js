7c86aa81bd22cba52d86792efaa844e5c7b656cb
bayel ibraev <bayelibraev1@gmail.com>
2021-12-14 22:07:31 +0600

    landing

---
+++ landing/gulpfile.js
@@ -0,0 +1,95 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+const spritesmith = require('gulp.spritesmith');
+const del = require('del');
+
+
+
+//  server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            port:9000,
+            baseDir: "build"
+        }
+    });
+    gulp.watch('build/**/*').on('change', browserSync.reload);
+});
+
+// pug
+gulp.task('templates:compile', function buildHTML(){
+    return gulp.src('source/template/index.pug')
+      .pipe(
+        pug({
+        pretty:true
+        })
+      )
+      .pipe(gulp.dest('build'))
+  });
+
+  //sass
+  gulp.task('sass', function (){
+    return gulp.src('source/styles/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('build/css'));
+  });
+
+  //sprite
+ 
+gulp.task('sprite', function (cb) {
+    const spriteData = gulp.src('source/img/icons/*.png').pipe(spritesmith({
+      imgName: 'sprite.png',
+      imgPath: '../images/sprite.png',
+      cssName: 'sprite.scss'
+    }));
+    spriteData.img.pipe(gulp.dest('build/images/'));
+    spriteData.css.pipe(gulp.dest('source/styles/global/'));
+    cb();
+  });
+
+
+  //clean
+  gulp.task('clean', function (){
+    return del('build');
+    });
+
+
+
+
+
+   
+    gulp.task('fonts:copy', function (){
+    return gulp.src('./source/fonts/**/*.*')
+    pipe(gulp.dest('build/fonts'));
+    });
+  
+
+  gulp.task('copy:img', function (){
+    return gulp.src('./source/img/**/*.*')
+    pipe(gulp.dest('build/img'));
+    });
+    
+
+    gulp.task('copy', gulp.parallel('fonts:copy','copy:img')); 
+
+
+
+
+
+
+    //watchers
+    gulp.task('watch', function (){
+gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
+gulp.watch('source/styles/**/*.scss', gulp.series('sass'));
+    });
+    
+
+    gulp.task('default', gulp.series ('clean',
+    gulp.parallel('templates:compile','sass','sprite','copy'),
+    gulp.parallel('watch','server')
+    )
+    );
+    
+    
